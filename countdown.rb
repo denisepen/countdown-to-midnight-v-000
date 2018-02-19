c96926eed81c6ef98b5e8108c26812f436b7527d
Denise Pendleton <dpendl1@hotmail.com>
2018-02-19 02:02:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown (number)
-  # number = 10
-  while number.to_i > 0
+number = number.to_i
+  while number > 0
   puts "#{number} SECOND(S)!"
   number = number - 1
   sleep(1)
