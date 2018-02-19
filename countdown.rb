cccf993ee0e7ee5182d6c4cbccb43e52a8e59381
Denise Pendleton <dpendl1@hotmail.com>
2018-02-19 02:07:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,12 @@ number = number.to_i
 end
 
 def countdown_with_sleep
+    sleep(1)
   number = number.to_i
     while number > 0
     puts "#{number} SECOND(S)!"
     number = number - 1
-    sleep(1)
+
     end
    "HAPPY NEW YEAR!"
   end
