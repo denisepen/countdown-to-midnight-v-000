b9f1f18ddfea2cff39008a5b28bfb985e1728066
Denise Pendleton <dpendl1@hotmail.com>
2018-02-19 02:03:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,16 @@ number = number.to_i
   end
    puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  number = number.to_i
+    while number > 0
+    puts "#{number} SECOND(S)!"
+    number = number - 1
+    sleep(1)
+    end
+     puts "HAPPY NEW YEAR!"
+  end
+
 #
 # end
